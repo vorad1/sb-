25c9c6b175e571733922309a741d7abac1c7d7fd
vorad1 <64877335+vorad1@users.noreply.github.com>
2021-03-11 11:03:34 +1300

    Delete SBHSQL-Prj directory

--- SBHSQL-Prj/src/main/java/nz/ac/op/cs/SBHSQL/Prj/db/QuizUserRepository.java
+++
@@ -1,8 +0,0 @@
-package nz.ac.op.cs.SBHSQL.Prj.db;
-
-
-import nz.ac.op.cs.SBHSQL.Prj.models.QuizUser;
-import org.springframework.data.repository.CrudRepository;
-
-public interface QuizUserRepository  extends CrudRepository<QuizUser, String>{
-}
